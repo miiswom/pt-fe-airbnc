b1b838feafcb6d482b2596b355f9a511266818cb
Farmata Kane <kane.farmata@gmail.com>
2025-04-11 13:50:42 +0100

    Improve Header mobile view

--- src/components/Main/Header.jsx
+++ src/components/Main/Header.jsx
@@ -9,15 +9,15 @@ export default function Header() {
   const [hiddenClass, setHiddenClass] = useState(true)
 
   return (
-    <>
-      <nav className='container row'>
-        <h1 style={{ fontSize: "2em", color: "#BD003F", fontWeight: "bold" }}><Link to="/">AirBNC</Link></h1>
-        <div>
+    <nav className='container row'>
+      <div className=' row small-container'> 
+        <h1 style={{ fontSize: "2em", color: "#BD003F", fontWeight: "bold", padding: "20px 10px"}}><Link to="/">AirBNC</Link></h1>
+        </div>
+        <div className=' small-container' style={{alignSelf: "center"}}>
         <MenuDropout hiddenClass={hiddenClass} setHiddenClass={setHiddenClass}/>
         <LoginOptions hiddenClass={hiddenClass} setHiddenClass={setHiddenClass}/>
         </div>
-      </nav>
-    </>
+    </nav>
 
   )
 }
