6d238197daec3fbf87715130667a2a7bff8834e2
Farmata Kane <kane.farmata@gmail.com>
2025-04-28 19:03:26 +0100

    Add link to properties to h2 in PropertiesList

--- src/components/Properties/PropertiesList.jsx
+++ src/components/Properties/PropertiesList.jsx
@@ -8,7 +8,7 @@ export default function PropertiesList({ properties }) {
       {properties.map((property) => {
         return <li className="property-card" key={property.property_id}>
           <Link to={`/properties/${property.property_id}`}><img className="property-card-img" style={{ maxWidth: "300px" }} src={property.images[0]} alt="" /></Link>
-          <h2 style={{margin: "8px auto", fontSize: "1.2em"}}>{property.property_name}</h2>
+          <h2 style={{margin: "8px auto", fontSize: "1.2em"}}><Link to={`/properties/${property.property_id}`}>{property.property_name}</Link></h2>
           <p>GBP {property.price_per_night} / night</p>
         </li>
       })}
