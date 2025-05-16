edebb9a986eb06c0f070bb8641c72812559e50e7
Farmata Kane <kane.farmata@gmail.com>
2025-05-16 11:14:54 +0100

    Update SKeletonProperty for mobile view

--- src/components/Property/SkeletonProperty.jsx
+++ src/components/Property/SkeletonProperty.jsx
@@ -2,12 +2,12 @@ export default function SkeletonProperty() {
   
     return (
     <>
-      <div className='container property'>
-        <h2 className="skeleton-grid" style={{width: "50%", fontSize: "2em", margin: "10px auto"}}>&nbsp;</h2>
-        <div className='skeleton-grid' style={{width: "40px", height:"30px", margin: "20px auto"}}>&nbsp;</div>
-        <div className='skeleton-grid' style={{maxWidth: "60%", height: "500px", margin: "10px auto"}}></div>
-        <div className="skeleton-grid" style={{width: "50%", fontSize: "2em", margin: "10px auto"}}>&nbsp;</div>
-        <div className="skeleton-grid" style={{width: "50%", fontSize: "2em", margin: "10px auto"}}>&nbsp;</div>
+      <div className=' property'>
+        <div className="skeleton-grid" style={{maxWidth: "600px", fontSize: "2em", margin: "10px auto"}}>&nbsp;</div>
+        <div className='skeleton-grid' style={{maxWidth: "40px", height:"30px", margin: "20px auto"}}>&nbsp;</div>
+        <div className="skeleton-grid" style={{ margin: "10px auto", maxWidth: "800px", minHeight:"70vmin", alignItems: "center", justifyContent: "space-between", gap: "70%" }}></div>
+        <div className="skeleton-grid" style={{maxWidth: "500px", fontSize: "2em", margin: "13px auto"}}>&nbsp;</div>
+        <div className="skeleton-grid" style={{maxWidth: "500px", fontSize: "2em", margin: "13px auto"}}>&nbsp;</div>
       </div>
     </>)
 }
